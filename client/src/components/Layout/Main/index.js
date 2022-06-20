2364172daccb2e7b539527da9026f090b0eea813
unknown <benjoquilario@gmail.com>
2022-06-20 17:38:20 +0800

    cleaning up some unnecessary import and props

--- client/src/components/Layout/Main/index.js
+++ client/src/components/Layout/Main/index.js
@@ -1,23 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Posts from '../../Posts';
 import Search from '../../Search';
 
 import Share from '../../Share';
 
-const Main = ({ hasMore, currentId, setCurrentId, setPage, children }) => {
-  return (
-    <div className="col-span-full lg:col-span-9 xl:col-span-6">
-      <Search />
-      <Share />
-      <Posts
-        hasMore={hasMore}
-        currentId={currentId}
-        setCurrentId={setCurrentId}
-        setPage={setPage}
-      />
-      {children}
-    </div>
-  );
-};
+const Main = ({ hasMore, currentId, setCurrentId, setPage, children }) => (
+  <div className="col-span-full lg:col-span-9 xl:col-span-6">
+    <Search />
+    <Share />
+    <Posts
+      hasMore={hasMore}
+      currentId={currentId}
+      setCurrentId={setCurrentId}
+      setPage={setPage}
+    />
+    {children}
+  </div>
+);
 
 export default Main;
