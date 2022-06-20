19e3a4c9c2e996d346dafb044c3e81147f13c31a
unknown <benjoquilario@gmail.com>
2022-06-20 17:29:49 +0800

    add: logout actions

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,14 +1,14 @@
-import * as ActionType from '../constants/ActionTypes';
+import * as ActionTypes from '../constants/ActionTypes';
 import * as api from '../api';
 
 export const signin = (formData, navigate, location) => async dispatch => {
   try {
-    dispatch({ type: ActionType.LOADING });
+    dispatch({ type: ActionTypes.LOADING });
 
     const { data } = await api.signIn(formData);
 
-    dispatch({ type: ActionType.LOADING_DONE });
-    dispatch({ type: ActionType.AUTH, data });
+    dispatch({ type: ActionTypes.LOADING_DONE });
+    dispatch({ type: ActionTypes.AUTH, data });
     navigate('/', location.search);
   } catch (error) {
     console.log(error);
@@ -17,15 +17,20 @@ export const signin = (formData, navigate, location) => async dispatch => {
 
 export const signup = (formData, navigate, location) => async dispatch => {
   try {
-    dispatch({ type: ActionType.LOADING });
+    dispatch({ type: ActionTypes.LOADING });
 
     const { data } = await api.signUp(formData);
 
-    dispatch({ type: ActionType.LOADING_DONE });
-    dispatch({ type: ActionType.AUTH, data });
+    dispatch({ type: ActionTypes.LOADING_DONE });
+    dispatch({ type: ActionTypes.AUTH, data });
     navigate('/', location.search);
   } catch (error) {
     const errors = error.response?.data.errors;
-    dispatch({ type: ActionType.ERR0R, payload: errors });
+    dispatch({ type: ActionTypes.ERR0R, payload: errors });
   }
+};
+
+export const logout = (navigate, location) => async dispatch => {
+  dispatch({ type: ActionTypes.LOGOUT });
+  navigate('/auth', location.search);
 };
