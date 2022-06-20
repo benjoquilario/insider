96ae8567b5b0edee3eb35cbab00260c754c4f834
unknown <benjoquilario@gmail.com>
2022-06-20 17:31:04 +0800

    add: getProfile functions

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,13 +1,13 @@
-import * as ActionType from '../constants/ActionTypes';
+import * as ActionTypes from '../constants/ActionTypes';
 import * as api from '../api';
 
 export const getProfile = id => async dispatch => {
   try {
-    dispatch({ type: ActionType.LOADING });
+    dispatch({ type: ActionTypes.LOADING });
 
     const { data } = await api.fetchProfile(id);
 
-    dispatch({ type: ActionType.FETCH_PROFILE, payload: { user: data } });
+    dispatch({ type: ActionTypes.FETCH_PROFILE, payload: { user: data } });
   } catch (error) {
     console.log(error);
   }
