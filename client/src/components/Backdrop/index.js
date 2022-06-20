3c340789e20ce9bd47572d442f715468742c3be8
unknown <benjoquilario@gmail.com>
2022-06-20 17:33:14 +0800

    add a function by clicking a backdrop to close the modal

--- client/src/components/Backdrop/index.js
+++ client/src/components/Backdrop/index.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { backdropVariant } from '../../utilities/framerVariant';
 
-const Backdrop = ({ handleClickOutside, children }) => {
-  return (
-    <AnimatePresence>
-      <motion.div
-        variants={backdropVariant}
-        initial="hidden"
-        animate="visible"
-        exit="hidden"
-        onClick={handleClickOutside}
-        className="fixed top-0 left-0 flex items-center justify-center w-full min-h-screen z-50 bg-gradient-to-r from-[#00000080] to-[#00000080] animate-[opacity_0.5s_ease-in-out]"
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
-  );
-};
+const Backdrop = ({ handleClickOutside, children }) => (
+  <AnimatePresence>
+    <motion.div
+      variants={backdropVariant}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      onClick={handleClickOutside}
+      className="fixed top-0 left-0 flex items-center justify-center w-full min-h-screen z-50 bg-gradient-to-r from-[#00000080] to-[#00000080] animate-[opacity_0.5s_ease-in-out]"
+    >
+      {children}
+    </motion.div>
+  </AnimatePresence>
+);
 
 export default Backdrop;
