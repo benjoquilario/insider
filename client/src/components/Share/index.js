b633ccf1205c1db4c4f26553a44c91f25e40e58c
unknown <benjoquilario@gmail.com>
2022-06-20 17:44:51 +0800

    Share post components

--- client/src/components/Share/index.js
+++ client/src/components/Share/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import defaultImage from '../../assets/images/default-image.png';
 import capitalizeName from '../../utilities/capitalizeName';
@@ -11,11 +12,17 @@ const Share = () => {
   return (
     <div className="mt-4 relative h-20 bg-gray-800 flex justify-start items-center rounded p-2 px-1 md:px-3 gap-1 overflow-hidden">
       <div className="min-h-6 w-20 max-w-20">
-        <img
-          className="w-11 h-11 rounded-full object-cover m-auto"
-          src={defaultImage}
-          alt="avatar"
-        />
+        <Link
+          aria-label={user?.result?.name}
+          to={`profile/${user?.result?._id}`}
+          tabIndex={0}
+        >
+          <img
+            className="w-11 h-11 rounded-full object-cover m-auto"
+            src={defaultImage}
+            alt="avatar"
+          />
+        </Link>
       </div>
       <Button
         onClickHandler={() =>
@@ -25,6 +32,7 @@ const Share = () => {
           })
         }
         classes="w-full h-11 text-gray-400 flex items-center justify-start bg-gray-900 mr-1 p-3 rounded-full shadow-[0_0_12px_-5px_rgba(0,0,0,0.2)] hover:bg-gray-700"
+        ariaLabel="create a post"
       >
         <span className="ml-2 text-xs md:text-sm">
           What's on your mind,
