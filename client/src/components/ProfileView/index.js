3cb0d9a5fce7c1730679c6e97eee852005cc0ec6
unknown <benjoquilario@gmail.com>
2022-06-20 17:48:45 +0800

    implementing a profile page

---
+++ client/src/components/ProfileView/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProfileView = () => {
+  return (
+    <div className="col-span-full lg:col-span-9 xl:col-span-6">ProfileView</div>
+  );
+};
+
+export default ProfileView;
--- client/src/pages/profile/index.js
+++ client/src/pages/profile/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from '../../components/Layout/Header';
+import Right from '../../components/Layout/Right';
+import ProfileView from '../../components/ProfileView';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="grid grid-cols-12 gap-6 max-w-6xl w-full mx-auto bg-gray-900 h-full p-3 md:p-5">
+      <Header />
+      <ProfileView />
+      <Right />
+    </div>
+  );
 };
 
 export default Profile;
