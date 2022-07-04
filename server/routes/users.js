60f3f09b0d16df8f1ef81af75f986d79d443b19e
unknown <benjoquilario@gmail.com>
2022-07-05 00:44:06 +0800

    add routes for following followers and search

---
+++ server/routes/users.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import {
+  updateProfile,
+  getUserWithPost,
+  getAllUsers,
+  getUser,
+  getFollowersById,
+  getFollowingById,
+  followUser,
+  unFollowUser,
+  searchUser,
+} from '../controllers/users.js';
+
+const router = express.Router();
+
+import auth from '../middleware/auth.js';
+
+router.patch('/:id/updateProfile', auth, updateProfile);
+router.get('/:id/posts', auth, getUserWithPost);
+router.get('/:id/user', auth, getUser);
+router.get('/users/all', auth, getAllUsers);
+router.patch('/:id/follow', auth, followUser);
+router.patch('/:id/unfollow', auth, unFollowUser);
+router.get('/:id/followers', auth, getFollowersById);
+router.get('/:id/following', auth, getFollowingById);
+router.get('/search', auth, searchUser);
+
+export default router;
