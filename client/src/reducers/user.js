4998d1b438d3f35734f4f5c226cb5329b88a74bd
unknown <benjoquilario@gmail.com>
2022-07-05 00:59:26 +0800

    make a usersloading and user information

--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -1,16 +1,46 @@
-import * as ActionTypes from '../constants/ActionTypes';
+import * as TYPES from '../constants/ActionTypes';
 
 const initialState = {
   loading: false,
   userLoading: false,
-  user: { followers: [] },
+  user: {},
   searchUser: [],
+  users: [],
+  followers: [],
+  page: 1,
 };
 
 const userReducers = (state = initialState, action) => {
   switch (action.type) {
-    case ActionTypes.FETCH_PROFILE:
+    case TYPES.FETCH_PROFILE:
+      return { ...state, loading: false, user: action.payload };
+    case TYPES.FETCH_ALL_PROFILE:
+      return {
+        ...state,
+        loading: false,
+        users: [...state.users, ...action.payload.data.map(user => user)],
+      };
+    case TYPES.UPDATE_PROFILE:
       return { ...state, user: action.payload };
+    case TYPES.FOLLOW_USER:
+      return {
+        ...state,
+        user: { ...state.user, followers: action.payload.followers },
+      };
+    case TYPES.FOLLOWERS_BY_ID:
+      console.log(action.payload);
+      return {
+        ...state,
+        followers: action.payload,
+      };
+    case TYPES.START_LOADING:
+      return { ...state, loading: true };
+    case TYPES.ADD_PAGE:
+      return { ...state, page: state.page + 1 };
+    case TYPES.SET_PAGE_TO_DEFAULT:
+      return { ...state, page: 1 };
+    case TYPES.CLEAR_USERS:
+      return { ...state, users: [] };
     default:
       return state;
   }
