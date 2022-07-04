e34ef7a475dee5a0d71b5ac7a7e27a45b53dbc7f
unknown <benjoquilario@gmail.com>
2022-07-05 00:41:58 +0800

    add followers and following models

--- server/models/user.js
+++ server/models/user.js
@@ -1,47 +1,56 @@
 import mongoose from 'mongoose';
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
-    trim: true,
-    maxlength: 25,
-  },
-  email: {
-    type: String,
-    require: true,
-    trim: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    require: true,
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-  updatedAt: {
-    type: Date,
-    default: new Date(),
-  },
-  imageUrl: {
-    type: String,
-    default:
-      'https://apsec.iafor.org/wp-content/uploads/sites/37/2017/02/IAFOR-Black-Avatar-Image.jpg',
-  },
-  coverPhoto: {
-    type: String,
-  },
-  followers: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: 'user',
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      trim: true,
+      maxlength: 25,
     },
-  ],
-  id: {
-    type: String,
+    email: {
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+    updatedAt: {
+      type: Date,
+      default: new Date(),
+    },
+    imageUrl: {
+      type: String,
+    },
+    coverPhoto: {
+      type: String,
+    },
+    followers: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    id: {
+      type: String,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model('User', userSchema);
