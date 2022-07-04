9de90d4ccd443af3beff64604c5bdc728a932690
unknown <benjoquilario@gmail.com>
2022-07-05 00:41:24 +0800

    add: comments

--- server/models/postsMessage.js
+++ server/models/postsMessage.js
@@ -1,37 +1,40 @@
 import mongoose from 'mongoose';
 
-const postSchema = mongoose.Schema({
-  name: String,
-  message: String,
-  creator: String,
-  selectedFile: String,
-  likes: {
-    type: [String],
-    default: [],
-  },
-  users: [
-    {
+const postSchema = mongoose.Schema(
+  {
+    name: String,
+    message: String,
+    creator: String,
+    selectedFile: String,
+    likes: {
+      type: [String],
+      default: [],
+    },
+    users: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
     },
-  ],
-  comments: [
-    {
-      _id: { type: String, require: true },
-      user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-      comment: { type: String, require: true },
-      createdAt: { type: Date, default: new Date() },
+    comments: [
+      {
+        _id: { type: String, require: true },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        comment: { type: String, require: true },
+        createdAt: { type: Date, default: Date.now() },
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: new Date(),
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: new Date(),
+    updatedAt: {
+      type: Date,
+      default: new Date(),
+    },
   },
-  updatedAt: {
-    type: Date,
-    default: new Date(),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const postMessage = mongoose.model('PostMessage', postSchema);
 
