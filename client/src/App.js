8f25c4aaa04642352be57018c011111caa838ee1
unknown <benjoquilario@gmail.com>
2022-07-05 00:50:21 +0800

    add a route for following and followers

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Auth from './pages/auth';
 import Profile from './pages/profile';
+import Followers from './pages/followers';
+import ProtectedRoutes from './pages/protected/ProtectedRoute';
+import Layout from './pages/layout/layout';
 import './index.css';
 
 const App = () => {
@@ -10,9 +13,14 @@ const App = () => {
     <div className="flex h-full min-h-screen bg-gray-900">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/" element={<Layout />}>
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/" element={<Home />} />
+              <Route path="home" element={<Home />} />
+              <Route path="profile/:id" element={<Profile />} />
+              <Route path="followers" element={<Followers />} />
+            </Route>
+          </Route>
           <Route path="/auth" element={<Auth />} />
         </Routes>
       </Router>
