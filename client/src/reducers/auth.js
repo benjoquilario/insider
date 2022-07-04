e394be60c32a969490268756cc9715eb301f42ce
unknown <benjoquilario@gmail.com>
2022-07-05 00:58:59 +0800

    make a usersloading and user information

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,21 +1,34 @@
-import * as ActionTypes from '../constants/ActionTypes';
+import * as TYPES from '../constants/ActionTypes';
 
-const authReducer = (state = { user: null }, action) => {
+const initialState = {
+  user: null,
+  userLoading: false,
+  isLoading: false,
+  errors: null,
+};
+
+const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ActionTypes.AUTH:
-      localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
-      return { ...state, user: action?.data };
-    case ActionTypes.LOGOUT:
-      localStorage.removeItem('profile', `${action?.data?.token}`);
-      return { ...state, user: null, loading: false, errors: null };
-    case ActionTypes.ERR0R:
-      return { ...state, loading: false, error: action.payload };
-    case ActionTypes.LOADING:
-      return { ...state, loading: true, error: null };
-    case ActionTypes.LOADING_DONE:
-      return { ...state, loading: false, error: null };
-    case ActionTypes.CLEAR_ERROR:
-      return { ...state, error: null };
+    case TYPES.INSERT_USER:
+      return { ...state, userLoading: false, user: action.payload };
+    case TYPES.AUTH:
+      localStorage.setItem('profile', JSON.stringify({ ...action.payload }));
+      return { ...state, user: action.payload.result };
+    case TYPES.LOGOUT:
+      localStorage.removeItem('profile', `${action?.payload?.token}`);
+      return { ...state, user: null, loading: false, error: null };
+    case TYPES.ERROR:
+      return { ...state, userLoading: false, errors: action.payload };
+    case TYPES.USER_LOADING:
+      return { ...state, userLoading: true, errors: null };
+    case TYPES.FINISH_USER_LOADING:
+      return { ...state, userLoading: false, errors: null };
+    case TYPES.AUTH_START_LOADING:
+      return { ...state, isLoading: true, errors: null };
+    case TYPES.AUTH_FINISH_LOADING:
+      return { ...state, isLoading: false, errors: null };
+    case TYPES.CLEAR_ERROR:
+      return { ...state, errors: null };
     default:
       return state;
   }
