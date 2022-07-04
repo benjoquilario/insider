92a4292b2ded6e598305a5d6087b7ab3ce6c931d
unknown <benjoquilario@gmail.com>
2022-07-05 00:54:50 +0800

    api for fetching comment, followers following and authenticated user

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -19,16 +19,29 @@ export const fetchPosts = page => API.get(`/posts?page=${page}`);
 export const createPost = newPost => API.post('/posts', newPost);
 export const updatePost = (id, updatedPost) =>
   API.patch(`posts/${id}`, updatedPost);
-export const deletePost = id => API.delete(`posts/${id}`);
-export const likePost = id => API.patch(`posts/${id}/likePost`);
+export const deletePost = id => API.delete(`/posts/${id}`);
+export const likePost = id => API.patch(`/posts/${id}/likePost`);
 
-export const updateProfile = (id, updatedProfile) =>
-  API.patch(`profile/${id}/updateProfile`, updatedProfile);
+export const updateProfile = (id, formData) =>
+  API.patch(`/users/${id}/updateProfile`, formData);
 
-export const fetchProfile = id => API.get(`/profile/${id}`);
+export const fetchUserWithPost = (id, page) =>
+  API.get(`/users/${id}/posts?page=${page}`);
+export const fetchAllProfile = page => API.get(`/users?page=${page}`);
+export const fetchUser = id => API.get(`/users/${id}/user`);
+export const fetchAllUsers = page => API.get(`/users/users/all?page=${page}`);
 
 export const postComment = (id, postComment) =>
   API.post(`/posts/${id}/comment`, { postComment });
+export const updateComment = (id, commentData) =>
+  API.patch(`/posts/${id}/updateComment`, { commentData });
+export const deleteComment = id => API.delete(`/posts/${id}/deleteComment`);
 
-export const signIn = formData => API.post('/user/signin', formData);
-export const signUp = formData => API.post('/user/signup', formData);
+export const followUser = id => API.patch(`/users/${id}/follow`);
+export const unFollowUser = id => API.patch(`/users/${id}/unfollow`);
+export const fetchFollowersById = id => API.get(`/users/${id}/followers`);
+export const fetchFollowingById = id => API.get(`/users/${id}/following`);
+
+export const signIn = formData => API.post('/auth/signin', formData);
+export const signUp = formData => API.post('/auth/signup', formData);
+export const authUser = () => API.get(`/auth/login/user`);
