655086210fb9349ea9a3c27263d1b90d3550e956
unknown <benjoquilario@gmail.com>
2022-07-05 00:49:16 +0800

    remove strictmode

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './App';
-
 import { reducers } from './reducers';
 import './index.css';
 
