1c5b18f662282a3730f4258b36ed004b55cea86d
unknown <benjoquilario@gmail.com>
2022-07-05 20:54:09 +0800

    implementing a search name functionality

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BiSearch } from 'react-icons/bi';
 
 const Search = () => {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = event => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="relative">
+    <div className="relative" onSubmit={handleSearch}>
       <form className="relative">
         <div aria-live="polite" className="sr-only"></div>
         <input
           name="search"
           type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
           placeholder="Search for a user..."
-          className="text-white rounded w-full pl-11 p-3 bg-gray-800"
+          className="text-sm md:text-base text-white rounded w-full pl-11 p-3 bg-gray-800"
           autoComplete="off"
         />
         <label className="sr-only" htmlFor="search">
           seach for a user...
           <span className="sr-only">and press enter</span>
         </label>
-        <Link to="/search?user">
+        <Link to={`http://localhost:5000/users/search?q=${query}`}>
           <button
             type="submit"
             className="text-white absolute top-0 bottom-0 flex items-center p-3"
