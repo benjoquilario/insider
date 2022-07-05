d2fa114d64fee2c33303d50b433c0a086524cf15
unknown <benjoquilario@gmail.com>
2022-07-05 20:52:38 +0800

    change post to posts and user to users name

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,12 @@ import { combineReducers } from 'redux';
 
 import posts from './posts';
 import auth from './auth';
-import user from './user';
+import users from './user';
 import modal from './modal';
 
-export const reducers = combineReducers({ posts, auth, user, modal });
+export const reducers = combineReducers({
+  posts,
+  auth,
+  users,
+  modal,
+});
