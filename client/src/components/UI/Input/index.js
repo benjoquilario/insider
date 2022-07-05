1e2e872914cf235689793b99fca9f92782f3492b
unknown <benjoquilario@gmail.com>
2022-07-06 00:33:15 +0800

    reduce the border of each input field

--- client/src/components/UI/Input/index.js
+++ client/src/components/UI/Input/index.js
@@ -10,7 +10,7 @@ const Input = ({ label, name, placeholder, type, handleChange, classes }) => (
       type={type}
       name={name}
       placeholder={placeholder}
-      className={`bg-white rounded text-[#9FADBD] text-sm font-semibold leading-[40px] h-[40px] px-[15px] w-full outline-none ${classes}`}
+      className={`bg-white rounded text-gray-700 text-sm font-normal leading-[40px] h-[40px] px-[15px] w-full outline-none ${classes}`}
       aria-required="true"
       aria-describedby={name}
     />
--- client/src/pages/auth/index.js
+++ client/src/pages/auth/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
+import Ripples from 'react-ripples';
 import { signin, signup } from '../../actions/auth';
 import Input from '../../components/UI/Input';
 import AuthLoading from '../../components/UI/Loading/AuthLoading';
 import { initialForm } from '../../utilities/intialState';
+import { CLEAR_ERROR } from '../../constants/ActionTypes';
 
 const Auth = () => {
   const { errors, isLoading } = useSelector(state => state.auth);
@@ -21,6 +23,8 @@ const Auth = () => {
 
   const switchMode = () => {
     setIsSignup(prevIsSignup => !prevIsSignup);
+    dispatch({ type: CLEAR_ERROR });
+    setFormData(initialForm);
   };
 
   const handleChange = event =>
@@ -37,12 +41,30 @@ const Auth = () => {
     <div className="flex items-center justify-center h-full w-full min-h-screen">
       <div className="flex flex-col items-center justify-center max-w-full md:max-w-[400px] mx-auto mt-0 min-h-[430px] md:min-h-auto bg-white w-4/5 md:w-full py-[20px] px-[24px] md:px-[40px] text-center rounded-md shadow-md">
         <h1 className="text-2xl mt-[13px] mb-[20px] font-semibold text-gray-900">
-          {isSignup ? 'Sign Up to Insider' : 'Sign In to Insider'}
+          {isSignup ? (
+            <div className="flex flex-col">
+              <span className="text-base font-light tracking-tight">
+                Create Account
+              </span>
+              <span className="text-lg font-normal tracking-wide">
+                Create a new account
+              </span>
+            </div>
+          ) : (
+            <div className="flex flex-col ">
+              <span className="text-base font-light tracking-tight">
+                Welcome to Back!
+              </span>
+              <span className="text-lg font-normal tracking-wide">
+                Sign in to your account
+              </span>
+            </div>
+          )}
         </h1>
         <form className="w-full" onSubmit={handleSubmit}>
           <div className="flex flex-col items-center justify-center">
             {isSignup && (
-              <div className="flex gap-2">
+              <div className="flex gap-2 w-full">
                 <div className="flex flex-col items-start w-full mb-[8px]">
                   <Input
                     handleChange={handleChange}
@@ -50,7 +72,7 @@ const Auth = () => {
                     name="firstName"
                     label="First Name"
                     placeholder="First Name"
-                    classes={`border-2 border-gray-300 hover:border-gray-700 ${
+                    classes={`border border-gray-300 hover:border-gray-700 ${
                       errors?.firstName
                         ? 'border-red-500 focus:border-red-500'
                         : 'border-gray-300 focus:border-blue-500'
@@ -66,7 +88,7 @@ const Auth = () => {
                     name="lastName"
                     label="Last Name"
                     placeholder="Last Name"
-                    classes={`border-2 border-gray-300 hover:border-gray-700 ${
+                    classes={`border border-gray-300 hover:border-gray-700 ${
                       errors?.lastName
                         ? 'border-red-500 focus:border-red-500'
                         : 'border-gray-300 focus:border-blue-500'
@@ -83,7 +105,7 @@ const Auth = () => {
                 name="email"
                 label="Email"
                 placeholder="Email"
-                classes={`border-2 border-gray-300 hover:border-gray-700 ${
+                classes={`border border-gray-300 hover:border-gray-700 ${
                   errors?.email
                     ? 'border-red-500 focus:border-red-500'
                     : 'border-gray-300 focus:border-blue-500'
@@ -98,13 +120,15 @@ const Auth = () => {
                 placeholder="Password"
                 label="Password"
                 type="password"
-                classes={`border-2 border-gray-300 hover:border-gray-700 ${
+                classes={`border border-gray-300 hover:border-gray-700 ${
                   errors?.password
                     ? 'border-red-500 focus:border-red-500'
                     : 'border-gray-300 focus:border-blue-500'
                 }`}
               />
-              <p className="text-red-400 text-xs">{errors?.password}</p>
+              {errors?.password && (
+                <p className="text-red-400 text-xs">{errors?.password}</p>
+              )}
             </div>
             {isSignup && (
               <div className="flex flex-col items-start w-full mb-[8px]">
@@ -114,7 +138,7 @@ const Auth = () => {
                   name="confirmPassword"
                   placeholder="Confirm Password"
                   type="password"
-                  classes={`border-2 border-gray-300 hover:border-gray-700 ${
+                  classes={`border border-gray-300 hover:border-gray-700 ${
                     errors?.password
                       ? 'border-red-500 focus:border-red-500'
                       : 'border-gray-300 focus:border-blue-500'
@@ -123,20 +147,24 @@ const Auth = () => {
                 <p className="text-red-400 text-xs">{errors?.password}</p>
               </div>
             )}
-            <button
-              type="submit"
-              className="inline-block bg-[#6a55fa] hover:bg-[#816ffb] text-sm font-semibold py-[9px] px-[30px] mt-2 w-full rounded text-white text-center focusBtn"
-            >
-              {isLoading ? (
-                <AuthLoading />
-              ) : (
-                <>{isSignup ? 'Sign Up' : 'Sign In'}</>
-              )}
-            </button>
+            <div className="mt-3 w-full">
+              <Ripples color="#ffffff80" className="w-full">
+                <button
+                  type="submit"
+                  className="inline-block bg-[#6a55fa] hover:bg-[#816ffb] text-sm font-semibold py-[9px] px-[30px] w-full rounded text-white text-center focusBtn"
+                >
+                  {isLoading ? (
+                    <AuthLoading />
+                  ) : (
+                    <>{isSignup ? 'Sign Up' : 'Sign In'}</>
+                  )}
+                </button>
+              </Ripples>
+            </div>
             <button
               type="button"
               onClick={switchMode}
-              className="flex items-center justify-center text-sm mt-[30px] text-[#8596A5]"
+              className="flex items-center justify-center text-xs mt-[30px] text-[#8596A5]"
             >
               {isSignup ? (
                 <>
