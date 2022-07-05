7d24b2cc4d2419e87c3f4cc284a2c15cf2f5bfea
unknown <benjoquilario@gmail.com>
2022-07-06 00:35:07 +0800

    add routes for following and search

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import Home from './pages/home';
 import Auth from './pages/auth';
 import Profile from './pages/profile';
 import Followers from './pages/followers';
+import Following from './pages/following';
 import ProtectedRoutes from './pages/protected/ProtectedRoute';
 import Layout from './pages/layout/';
+import Search from './pages/search';
 import './index.css';
 
 const App = () => {
@@ -19,6 +21,8 @@ const App = () => {
               <Route path="home" element={<Home />} />
               <Route path="profile/:id" element={<Profile />} />
               <Route path="followers" element={<Followers />} />
+              <Route path="following" element={<Following />} />
+              <Route path="search" element={<Search />} />
             </Route>
           </Route>
           <Route path="/auth" element={<Auth />} />
