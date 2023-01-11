4df9d1caac5cb95bfe5a637c16f74c05c9567fe1
iambenjo <82529126+benjoquilario@users.noreply.github.com>
2023-01-11 15:44:09 +0800

    remove userall auth for testing

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ import auth from '../middleware/auth.js';
 router.patch('/:id/updateProfile', auth, updateProfile);
 router.get('/:id/posts', auth, getUserWithPost);
 router.get('/:id/user', auth, getUser);
-router.get('/users/all', auth, getAllUsers);
+router.get('/users/all', getAllUsers);
 router.patch('/:id/follow', auth, followUser);
 router.patch('/:id/unfollow', auth, unFollowUser);
 router.get('/:id/followers', auth, getFollowersById);
